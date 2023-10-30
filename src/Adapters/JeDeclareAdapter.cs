50e8fdb3050b7749c4c55b7196e7f08352c53bad
Marwen Elleuch <melleuch@kpmg.fr>
2023-10-30 10:25:17 +0000

    Merged PR 26788: fix startup of the api
    fix startup of the api
    
    Related work items: #269004

---
+++ src/Adapters/JeDeclareAdapter.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="JeDeclareAdapter.cs" company="KPMG">
+// Copyright (c) KPMG. All rights reserved.
+// </copyright>
+
+namespace KPMG.Pulse.Back.Accounting.Mandate.Adapters
+{
+    public class JeDeclareAdapter : IJeDeclareService
+    {
+        public Task<Bban> AddRibToFolderAsync(string? bankServicesProviderId, Bban bban, Signatory signatory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Collection> CreateCollecteConfigurationAsync(string bankServicesProviderId, Bban rib)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> CreateFolderAsync(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

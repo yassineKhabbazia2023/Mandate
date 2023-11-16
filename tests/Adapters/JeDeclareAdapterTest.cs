581bd09aa246b0c4771b8b34be2566893a508ec8
Marwa CHEBIL <cmarwa@kpmg.fr>
2023-11-16 15:56:17 +0100

    Add missing UT

---
+++ tests/Adapters/JeDeclareAdapterTest.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="JeDeclareAdapterTest.cs" company="KPMG">
+// Copyright (c) KPMG. All rights reserved.
+// </copyright>
+
+namespace KPMG.Pulse.Back.Accounting.Mandate.Adapters.Tests
+{
+    using KPMG.Pulse.Back.Accounting.Mandate.JeDeclare.Client;
+
+    public class JeDeclareAdapterTest
+    {
+        [Fact]
+        public async Task GetMandatPdfAsync()
+        {
+            byte[] data = { 0, 16, 104, 213 };
+
+            var jedeclareClient = new Mock<IJeDeclareClient>(MockBehavior.Strict);
+            jedeclareClient.Setup(c => c.GetMandatPdfAsync("jdcCompteIdT", "jdcFolderIdT", "jdcRibIdT"))
+                .ReturnsAsync(data)
+                .Verifiable();
+
+            var adapter = new JeDeclareAdapter(jedeclareClient.Object);
+            var result = await adapter.GetMandatPdfAsync("jdcCompteIdT", "jdcFolderIdT", "jdcRibIdT");
+
+            result.Should().BeEquivalentTo(data);
+            jedeclareClient.VerifyAll();
+        }
+    }
+}

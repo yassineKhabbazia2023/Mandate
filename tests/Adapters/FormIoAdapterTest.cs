2b6b648701e716f8cbcd25c4afdfaa3739949dc4
melleuch <melleuch@kpmg.fr>
2023-12-07 11:15:54 +0100

    correction adapter

--- tests/Adapters/FormIoAdapterTest.cs
+++ tests/Adapters/FormIoAdapterTest.cs
@@ -54,7 +54,7 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.Adapters.Tests
             };
 
             var formIoClient = new Mock<IFormioClient>(MockBehavior.Strict);
-            formIoClient.Setup(item => item.GetSubmissionMandateAsync("13507", "00014", "31464482121", "77", 0, 1, It.IsAny<FormioAuthToken>()))
+            formIoClient.Setup(item => item.GetSubmissionMandateAsync("13507", "00014", "31464482121", "77", It.IsAny<FormioAuthToken>()))
                 .ReturnsAsync(sub)
                 .Verifiable();
 
@@ -66,8 +66,7 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.Adapters.Tests
             var address = new Address("12 RUE DES 2 NATIONS", string.Empty, "59250", "HALLUIN", "France");
             var signatory = new Signatory("m", "OLIVIER", "BRUNELAT", "brunelatolivier@gmail.com");
             var company = new Company(Guid.Empty, "SPORT FIT SAS", "83455379400019", "1000326214", "19820673", signatory, address);
-            Status status = new Status(CollectionStatus.Created, "created");
-            var collection = new Collection(Guid.Empty, "8909440", company, bban1, new DateTime(2019, 10, 10, 8, 54, 3), new DateTime(2019, 10, 10, 8, 54, 3), status);
+            var collection = new Collection(Guid.Empty, "8909440", company, bban1, new DateTime(2019, 10, 10, 8, 54, 3), new DateTime(2019, 10, 10, 8, 54, 3), null!);
 
             res.Should().BeEquivalentTo(collection);
 

b3da6224ac21e7f387d275dd6c59fa3ec77f6538
Marwa CHEBIL <cmarwa@kpmg.fr>
2023-12-15 09:20:38 +0100

    add FormIoOptionsTest UTs

---
+++ tests/Services.Formio.Client.Http/Clients/FormIoOptionsTest.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="FormIoOptionsTest.cs" company="KPMG">
+// Copyright (c) KPMG. All rights reserved.
+// </copyright>
+
+namespace KPMG.Pulse.Back.Accounting.Mandate.Formio.Client.Http.Tests
+{
+    public class FormIoOptionsTest
+    {
+        [Fact]
+        public void Constructor()
+        {
+            var options = new FormIoOptions();
+
+            options.BaseUri.Should().BeNull();
+            options.FormioApiKey.Should().BeNull();
+        }
+
+        [Fact]
+        public void FormIoOptions_ValueChanged()
+        {
+            var options = new FormIoOptions()
+            {
+                BaseUri = new Uri("https://toto.com"),
+                FormioApiKey = "formioApiKeyT",
+            };
+
+            options.BaseUri.Should().BeEquivalentTo(new Uri("https://toto.com/"));
+            options.FormioApiKey.Should().Be("formioApiKeyT");
+        }
+
+        [Fact]
+        public void FormIoOptions_Validate_WhenBaseUriIsNull()
+        {
+            var options = new FormIoOptions()
+            {
+                BaseUri = null,
+                FormioApiKey = "formioApiKeyT",
+            };
+
+            var action = () => options.Validate();
+            action.Should().Throw<Exception>().WithMessage("Instance of FormIoOptions is invalid, BaseUri is null");
+        }
+
+        [Fact]
+        public void FormIoOptions_Validate_WhenFormioApiKeyIsNull()
+        {
+            var options = new FormIoOptions()
+            {
+                BaseUri = new Uri("https://toto.com"),
+                FormioApiKey = null!,
+            };
+
+            var action = () => options.Validate();
+            action.Should().Throw<Exception>().WithMessage("Instance of FormIoOptions is invalid, FormioApiKey is null");
+        }
+    }
+}

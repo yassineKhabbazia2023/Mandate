ce91dce38fac732c872451ab1baf51a62462951b
melleuch <melleuch@kpmg.fr>
2024-01-16 15:13:21 +0100

    fix converting collection

--- src/Adapters/SqlExtensions.cs
+++ src/Adapters/SqlExtensions.cs
@@ -24,6 +24,17 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.Adapters
             return new Bank(source.BankCode, source.BankName, source.BankGroup, source.EbicsCardId, bankagreement);
         }
 
+        public static Bban ToBbanModel(this Sql.CollectionDb source)
+        {
+            return new Bban(
+                source.BankCode!,
+                source.BranchCode!,
+                source.AccountNumber!,
+                source.CheckDigits!,
+                source.JeDeclareCollection?.JdcRibId,
+                source.Bank?.ToModel());
+        }
+
         public static Collection ToModel(this Sql.CollectionDb source)
         {
             Company company = new Company(
@@ -35,22 +46,30 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.Adapters
                 source.Personal?.ToSignatory(),
                 source.Personal?.ToAdress());
 
-            Bank? bank = source.Bank?.ToModel();
-
-            Bban? bban = new Bban(source.BankCode!, source.BranchCode!, source.AccountNumber!, source.CheckDigits!, source.JeDeclareCollection?.JdcRibId, bank);
+            Bban? bban = source.ToBbanModel();
 
             var currentStatus = source.Statuses?.SingleOrDefault(i => i.IsCurrent);
             var creationStatus = source.Statuses?.SingleOrDefault(i => i.StatusCode == -1);
 
-            Status? status = currentStatus != null ? currentStatus.ToModel() : null;
+            if (currentStatus == null)
+            {
+                throw new ApplicationException($"{nameof(SqlExtensions)} - {nameof(ToModel)} : Error while parsing Collection currentStatus is null.");
+            }
+
+            if (creationStatus == null)
+            {
+                throw new ApplicationException($"{nameof(SqlExtensions)} - {nameof(ToModel)} : Error while parsing Collection creationStatus is null.");
+            }
+
+            Status? status = currentStatus.ToModel();
 
             return new Collection(
                 id: source.Id,
                 collectionServicesProviderId: source.JeDeclareCollection?.JdcReleveId,
                 company: company,
                 bban: bban,
-                creationDate: (creationStatus?.StatusDate!).Value,
-                modificationDate: (currentStatus?.StatusDate!).Value,
+                creationDate: GetCreationDate(creationStatus),
+                modificationDate: GetModificationDate(currentStatus, creationStatus),
                 status: status!);
         }
 
@@ -135,6 +154,23 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.Adapters
                 StatusCode = (int)JdcCollectionStatus.InitialCreate,
                 StatusDate = DateTime.UtcNow,
             };
+        }
+
+        private static DateTime GetCreationDate(Sql.StatusDb? creationStatus)
+        {
+            return creationStatus!.StatusDate!.Value;
+        }
+
+        private static DateTime GetModificationDate(Sql.StatusDb currentStatus, Sql.StatusDb creationStatus)
+        {
+            if (currentStatus.StatusCode == creationStatus.StatusCode)
+            {
+                return creationStatus.StatusDate!.Value;
+            }
+            else
+            {
+                return currentStatus.StatusDate!.Value;
+            }
         }
     }
 }

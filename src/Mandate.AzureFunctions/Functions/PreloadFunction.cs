18a9a1f366c046cdc7a5314bbd429f5def0710fc
melleuch <melleuch@kpmg.fr>
2024-01-30 14:43:03 +0100

    enlevé function de test

--- src/Mandate.AzureFunctions/Functions/PreloadFunction.cs
+++ src/Mandate.AzureFunctions/Functions/PreloadFunction.cs
@@ -5,14 +5,11 @@
 namespace KPMG.Pulse.Back.Accounting.Mandate.AzureFunctions
 {
     using System;
-    using System.IO;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Azure.WebJobs;
     using Microsoft.Azure.WebJobs.Extensions.Http;
     using Microsoft.Extensions.Logging;
-    using Newtonsoft.Json;
 
     public class PreloadFunction
     {
@@ -25,7 +22,7 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.AzureFunctions
         }
 
         [FunctionName("PreloadFunction")]
-        public async Task PreloadFunctionAsync([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task PreloadFunctionAsync([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             try
             {
@@ -44,26 +41,6 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.AzureFunctions
                 log.LogError(ex, ex.Message);
                 throw;
             }
-        }
-
-        [FunctionName("MountFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
         }
     }
 }

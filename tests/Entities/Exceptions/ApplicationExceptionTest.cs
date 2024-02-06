43b01793a97b24688707007196779cc448ab2b64
Medini <smedini@kpmg.fr>
2024-02-06 11:21:46 +0100

    test: increase the code coverage

---
+++ tests/Entities/Exceptions/ApplicationExceptionTest.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="ApplicationExceptionTest.cs" company="KPMG">
+// Copyright (c) KPMG. All rights reserved.
+// </copyright>
+
+namespace KPMG.Pulse.Back.Accounting.Mandate.Tests
+{
+    using Newtonsoft.Json;
+
+    public class ApplicationExceptionTest
+    {
+        [Fact]
+        public void DefaultConstructor_ShouldInstantiateWithDefaultMessage()
+        {
+            // Act
+            var exception = new ApplicationException();
+
+            // Assert
+            exception.Message.Should().Be("Exception of type 'KPMG.Pulse.Back.Accounting.Mandate.ApplicationException' was thrown.");
+        }
+
+        [Fact]
+        public void ConstructorWithMessage_ShouldSetMessage()
+        {
+            // Arrange
+            var customMessage = "Custom error message";
+
+            // Act
+            var exception = new ApplicationException(customMessage);
+
+            // Assert
+            exception.Message.Should().Be(customMessage);
+        }
+
+        [Fact]
+        public void ConstructorWithMessageAndInnerException_ShouldSetMessageAndInnerException()
+        {
+            // Arrange
+            var customMessage = "Custom error message";
+            var innerException = new Exception("Inner exception message");
+
+            // Act
+            var exception = new ApplicationException(customMessage, innerException);
+
+            // Assert
+            exception.Message.Should().Be(customMessage);
+            exception.InnerException.Should().Be(innerException);
+        }
+
+        [Fact]
+        public void NewtonsoftJson_Serialization_ShouldPreserveExceptionProperties()
+        {
+            // Arrange
+            var customMessage = "Custom error message";
+            var innerException = new Exception("Inner exception message");
+            var exception = new ApplicationException(customMessage, innerException);
+
+            string jsonString;
+            ApplicationException deserializedException;
+
+            // Act
+            jsonString = JsonConvert.SerializeObject(exception);
+            deserializedException = JsonConvert.DeserializeObject<ApplicationException>(jsonString!);
+
+            // Assert
+            deserializedException?.Message.Should().Be(customMessage);
+            deserializedException?.InnerException?.Message.Should().Be(innerException.Message);
+        }
+    }
+}
---
+++ tests/Services.JeDeclare.Client/Exceptions/JeDeclareApiExceptionTest.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="JeDeclareApiExceptionTest.cs" company="KPMG">
+// Copyright (c) KPMG. All rights reserved.
+// </copyright>
+
+namespace KPMG.Pulse.Back.Accounting.Mandate.JeDeclare.Client.Tests
+{
+    using Newtonsoft.Json;
+
+    public class JeDeclareApiExceptionTest
+    {
+        [Fact]
+        public void DefaultConstructor_ShouldInstantiateWithDefaultMessage()
+        {
+            // Act
+            var exception = new JeDeclareApiException();
+
+            // Assert
+            exception.Message.Should().Be("Exception of type 'KPMG.Pulse.Back.Accounting.Mandate.JeDeclare.Client.JeDeclareApiException' was thrown.");
+        }
+
+        [Fact]
+        public void ConstructorWithMessage_ShouldSetMessage()
+        {
+            // Arrange
+            var customMessage = "Custom error message";
+
+            // Act
+            var exception = new JeDeclareApiException(customMessage);
+
+            // Assert
+            exception.Message.Should().Be(customMessage);
+        }
+
+        [Fact]
+        public void ConstructorWithMessageAndInnerException_ShouldSetMessageAndInnerException()
+        {
+            // Arrange
+            var customMessage = "Custom error message";
+            var innerException = new Exception("Inner exception message");
+
+            // Act
+            var exception = new JeDeclareApiException(customMessage, innerException);
+
+            // Assert
+            exception.Message.Should().Be(customMessage);
+            exception.InnerException.Should().Be(innerException);
+        }
+
+        [Fact]
+        public void NewtonsoftJson_Serialization_ShouldPreserveExceptionProperties()
+        {
+            // Arrange
+            var customMessage = "Custom error message";
+            var innerException = new Exception("Inner exception message");
+            var exception = new JeDeclareApiException(customMessage, innerException);
+
+            string jsonString;
+            JeDeclareApiException deserializedException;
+
+            // Act
+            jsonString = JsonConvert.SerializeObject(exception);
+            deserializedException = JsonConvert.DeserializeObject<JeDeclareApiException>(jsonString!);
+
+            // Assert
+            deserializedException?.Message.Should().Be(customMessage);
+            deserializedException?.InnerException?.Message.Should().Be(innerException.Message);
+        }
+    }
+}

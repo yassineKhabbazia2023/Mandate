08dc8042c3de729b927e3492abaf059b825bcf8e
melleuch <melleuch@kpmg.fr>
2024-04-16 10:50:09 +0200

    tests unitaires

--- 01_API/tests/Application/Managers/FormioManagerTest.cs
+++ 01_API/tests/Application/Managers/FormioManagerTest.cs
@@ -63,5 +63,61 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.Application.Tests.Managers
 
             services.VerifyAll();
         }
+
+        [Fact]
+        public async Task GetAllCollectionAsync()
+        {
+            Bban bban = EntityFactory.Bban;
+            Status status = new Status(CollectionStatus.ToDo, "En Cours");
+            Collection collection = new Collection(
+                Guid.Empty,
+                "23414",
+                EntityFactory.Company,
+                bban,
+                new DateTime(2023, 8, 12),
+                new DateTime(2023, 8, 12),
+                status);
+
+            var services = new Mock<IFormioService>(MockBehavior.Strict);
+            services.Setup(item =>
+                item.GetAllCollectionAsync(0, 1))
+                .ReturnsAsync(new List<Collection> { collection })
+                .Verifiable();
+
+            var manager = new FormioManager(services.Object);
+
+            var res = await manager.GetAllCollectionAsync(0, 1);
+            res.Should().BeEquivalentTo(new List<Collection> { collection });
+
+            services.VerifyAll();
+        }
+
+        [Fact]
+        public async Task GetAllCollectionAsync_When_GetAllCollectionAsync_Throw_Exception()
+        {
+            Bban bban = EntityFactory.Bban;
+            Status status = new Status(CollectionStatus.ToDo, "En Cours");
+            Collection collection = new Collection(
+                Guid.Empty,
+                "23414",
+                EntityFactory.Company,
+                bban,
+                new DateTime(2023, 8, 12),
+                new DateTime(2023, 8, 12),
+                status);
+
+            var services = new Mock<IFormioService>(MockBehavior.Strict);
+            services.Setup(item =>
+                item.GetAllCollectionAsync(0, 1))
+                .ThrowsAsync(new Exception("message"))
+                .Verifiable();
+
+            var manager = new FormioManager(services.Object);
+
+            Func<Task> action = async () => await manager.GetAllCollectionAsync(0, 1);
+            await action.Should().ThrowExactlyAsync<Exception>().WithMessage("message");
+
+            services.VerifyAll();
+        }
     }
 }

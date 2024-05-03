75afb1ddf71d85e39e2c8f03b54bd5513e5d9b96
melleuch <melleuch@kpmg.fr>
2024-05-03 14:41:52 +0200

    test unitaire

--- 02_MandateFunction/tests/Mandate.AzureFunctions.Tests/Functions/RecoveryFormIOFunctionTest.cs
+++ 02_MandateFunction/tests/Mandate.AzureFunctions.Tests/Functions/RecoveryFormIOFunctionTest.cs
@@ -7,21 +7,18 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.AzureFunctions.Tests.Functions
     using global::Mandate.AzureFunctions;
     using KPMG.Pulse.Back.Accounting.Mandate.Function.Functions;
     using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Logging;
     using Newtonsoft.Json;
 
     public class RecoveryFormIOFunctionTest
     {
-        private readonly Mock<IDurableOrchestrationClient> mockStarter;
+        //private readonly Mock<IDurableOrchestrationClient> mockStarter;
         private readonly Mock<ILogger> mockLog;
-        private readonly Mock<IConfiguration> mockConfiguration;
 
         public RecoveryFormIOFunctionTest()
         {
-            this.mockStarter = new Mock<IDurableOrchestrationClient>(MockBehavior.Strict);
+            //this.mockStarter = new Mock<IDurableOrchestrationClient>(MockBehavior.Strict);
             this.mockLog = new Mock<ILogger>();
-            this.mockConfiguration = new Mock<IConfiguration>(MockBehavior.Strict);
         }
 
         [Fact]
@@ -43,43 +40,19 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.AzureFunctions.Tests.Functions
 
             Environment.SetEnvironmentVariable("LimitRecoveryFormIo", "100");
 
-            this.mockStarter.Setup(s => s.StartNewAsync(
+            var mockStarter = new Mock<IDurableOrchestrationClient>(MockBehavior.Strict);
+            mockStarter.Setup(s => s.StartNewAsync(
                     "RecoveryFormIoOrchestrator",
                     It.Is<RecoveryOrchestratorInput>(i => i.LimitConfig == 100 && i.Skip == 10)))
                 .ReturnsAsync(inst);
 
             HttpResponseMessage httpResponse = new HttpResponseMessage();
-            this.mockStarter.Setup(s => s.CreateCheckStatusResponse(httpRequestMessage, inst, false))
+            mockStarter.Setup(s => s.CreateCheckStatusResponse(httpRequestMessage, inst, false))
                 .Returns(httpResponse);
 
-            var response = await RecoveryFormIOFunction.HttpStart(httpRequestMessage, this.mockStarter.Object, this.mockLog.Object);
+            var response = await RecoveryFormIOFunction.HttpStart(httpRequestMessage, mockStarter.Object, this.mockLog.Object);
 
-            this.mockStarter.Verify(s => s.StartNewAsync("RecoveryFormIoOrchestrator", It.IsAny<object>()), Times.Once);
-        }
-
-        [Fact]
-        public async Task RecoveryFormIOFunction_HttpStart_WhenNoConfig_ShouldStartOrchestration()
-        {
-            string inst = "instanceId";
-            var content = new StringContent(JsonConvert.SerializeObject(null), Encoding.UTF8, "application/json");
-
-            var httpRequestMessage = new HttpRequestMessage()
-            {
-                Content = content,
-            };
-
-            this.mockStarter.Setup(s => s.StartNewAsync(
-                    "RecoveryFormIoOrchestrator",
-                    It.Is<RecoveryOrchestratorInput>(i => i.LimitConfig == 50 && i.Skip == 0)))
-                .ReturnsAsync(inst);
-
-            HttpResponseMessage httpResponse = new HttpResponseMessage();
-            this.mockStarter.Setup(s => s.CreateCheckStatusResponse(httpRequestMessage, inst, false))
-                .Returns(httpResponse);
-
-            var response = await RecoveryFormIOFunction.HttpStart(httpRequestMessage, this.mockStarter.Object, this.mockLog.Object);
-
-            this.mockStarter.Verify(s => s.StartNewAsync("RecoveryFormIoOrchestrator", It.IsAny<object>()), Times.Once);
+            mockStarter.Verify(s => s.StartNewAsync("RecoveryFormIoOrchestrator", It.IsAny<object>()), Times.Once);
         }
     }
 }

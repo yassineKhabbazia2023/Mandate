620bba901b9222c928ee4c8835d015b3f83c4b73
Rouge, Nicolas <nrouge@kpmg.fr>
2024-07-26 18:04:45 +0200

    Fix SiretNumber size

--- 01_API/src/Services.Sql.Implementation/Context/MandateContext.cs
+++ 01_API/src/Services.Sql.Implementation/Context/MandateContext.cs
@@ -99,7 +99,7 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.Sql.Implementation
                 .WithOne(jdf => jdf.Company)
                 .HasForeignKey<JeDeclareFolderDb>(jdf => jdf.CompanyId);
             modelBuilder.Entity<CompanyDb>().Property(c => c.Name).HasMaxLength(255).IsUnicode(true).IsRequired(false);
-            modelBuilder.Entity<CompanyDb>().Property(c => c.SiretNumber).IsFixedLength(true).HasMaxLength(14).IsRequired(false);
+            modelBuilder.Entity<CompanyDb>().Property(c => c.SiretNumber).IsFixedLength(true).HasMaxLength(150).IsRequired(false);
             modelBuilder.Entity<CompanyDb>().Property(c => c.ErpId).HasMaxLength(50).IsRequired(false);
 
             modelBuilder.Entity<PersonalDb>().HasKey(cp => cp.Id);
@@ -157,7 +157,7 @@ namespace KPMG.Pulse.Back.Accounting.Mandate.Sql.Implementation
 
             modelBuilder.Entity<MandateLogDb>().HasKey(c => c.Id);
             modelBuilder.Entity<MandateLogDb>().Property(c => c.ErpId).HasMaxLength(50).IsRequired(true);
-            modelBuilder.Entity<MandateLogDb>().Property(c => c.SiretNumber).IsFixedLength(true).HasMaxLength(150).IsRequired(true);
+            modelBuilder.Entity<MandateLogDb>().Property(c => c.SiretNumber).IsFixedLength(true).HasMaxLength(14).IsRequired(true);
             modelBuilder.Entity<MandateLogDb>().Property(cp => cp.BankCode).IsFixedLength(true).HasMaxLength(5).IsRequired(true);
             modelBuilder.Entity<MandateLogDb>().Property(cp => cp.BranchCode).IsFixedLength(true).HasMaxLength(5).IsRequired(true);
             modelBuilder.Entity<MandateLogDb>().Property(cp => cp.AccountNumber).IsFixedLength(true).HasMaxLength(11).IsRequired(true);

b01f7288a4f4b2ff40c8d0689d23970f297bcd48
Riantsoa Ramanampamonjy <rramanampamonjy@kpmg.fr>
2024-08-22 16:24:09 +0000

    Merged PR 33636: fix PostDeployment/AddCreationInprogressStatus.sql
    fix PostDeployment/AddCreationInprogressStatus.sql
    
    Related work items: #332362

--- 01_API/src/Database/PostDeployment/AddCreationInprogressStatus.sql
+++ 01_API/src/Database/PostDeployment/AddCreationInprogressStatus.sql
@@ -1,4 +1,4 @@
-﻿IF NOT EXISTS (SELECT StatusCode FROM Mandate.RefStatusCode)
+﻿IF NOT EXISTS (SELECT StatusCode FROM Mandate.RefStatusCode where StatusCode = 99)
 BEGIN
 INSERT into Mandate.RefStatusCode values (99,null,99,'','')
 END

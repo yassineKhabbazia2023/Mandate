2719485d169d46f058d7ef5b82c13e830f552e94
Dufour, Samuel <sdufour@kpmg.fr>
2024-12-19 16:45:09 +0100

    Update templates for non partner banks and refactor code #20579

--- 01_API/src/Entities/Models/Address.cs
+++ 01_API/src/Entities/Models/Address.cs
@@ -1,28 +1,24 @@
-﻿// <copyright file="Address.cs" company="KPMG">
-// Copyright (c) KPMG. All rights reserved.
-// </copyright>
-
-namespace KPMG.Pulse.Back.Accounting.Mandate
+﻿namespace KPMG.Pulse.Back.Accounting.Mandate;
+public class Address
 {
-    public class Address
+    public Address(string? street, string? complements, string? zipCode, string? city, string? country)
     {
-        public Address(string? street, string? complements, string? zipCode, string? city, string? country)
-        {
-            this.Street = street;
-            this.Complements = complements;
-            this.ZipCode = zipCode;
-            this.City = city;
-            this.Country = country;
-        }
-
-        public string? Street { get; }
-
-        public string? Complements { get; }
-
-        public string? ZipCode { get; }
-
-        public string? City { get; }
-
-        public string? Country { get; }
+        Street = street;
+        Complements = complements;
+        ZipCode = zipCode;
+        City = city;
+        Country = country;
     }
+
+    public string? Street { get; }
+
+    public string? Complements { get; }
+
+    public string? ZipCode { get; }
+
+    public string? City { get; }
+
+    public string? Country { get; }
+
+    public string FullAddress => string.Join(" - ", new[] { Street, Complements, ZipCode, City, Country }.Where(s => !string.IsNullOrEmpty(s)));
 }
